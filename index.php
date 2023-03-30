38c46101fd06fdaf4b0f2173878a80bf9fc9b4a2
ElmarAliyev-7 <liyevelmar02@gmail.com>
2023-03-30 12:49:52 +0400

    login page front section

--- index.php
+++ index.php
@@ -14,10 +14,9 @@
         </ol>
     </section>
     <section class="content">
-
         <div class="box">
             <div class="box-header with-border">
-                <h3 class="box-title">Title</h3>
+                <h3 class="box-title">Dashboard</h3>
                 <div class="box-tools pull-right">
                     <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Collapse">
                         <i class="fa fa-minus"></i></button>
@@ -28,13 +27,7 @@
             <div class="box-body">
                 Start creating your amazing application!
             </div>
-
-            <div class="box-footer">
-                Footer
-            </div>
-
         </div>
-
     </section>
 
     <!-- Main content -->
